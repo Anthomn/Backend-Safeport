979032f8c85b2d7297045ed518f5b9faa16c8811
Anedre <anedre12345@gmail.com>
2022-10-19 09:26:09 -0500

    Query Padre de familia

--- src/main/java/pe/edu/upc/tfsafeport/repositories/IPadredefamiliaRepository.java
+++ src/main/java/pe/edu/upc/tfsafeport/repositories/IPadredefamiliaRepository.java
@@ -1,10 +1,15 @@
 package pe.edu.upc.tfsafeport.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.tfsafeport.entities.Padredefamilia;
 
+import java.util.List;
+
 @Repository
 public interface IPadredefamiliaRepository extends JpaRepository<Padredefamilia, Integer> {
-
+    @Query("from Padredefamilia p where p.nombre like %:nombre")
+    List<Padredefamilia> search(@Param("namePropietario")String nombre);
 }
