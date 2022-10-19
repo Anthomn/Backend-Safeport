d9dba6ec3298cef342f805c01ba60eb38d1ba93d
Anedre <anedre12345@gmail.com>
2022-10-19 09:41:57 -0500

    Creacion de optional, delete y search

--- src/main/java/pe/edu/upc/tfsafeport/servicesimpls/UbicacionServiceImpl.java
+++ src/main/java/pe/edu/upc/tfsafeport/servicesimpls/UbicacionServiceImpl.java
@@ -7,6 +7,8 @@ import pe.edu.upc.tfsafeport.repositories.IUbicacionRepository;
 import pe.edu.upc.tfsafeport.servicesinterfaces.IUbicacionService;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UbicacionServiceImpl implements IUbicacionService {
 
@@ -21,5 +23,17 @@ public class UbicacionServiceImpl implements IUbicacionService {
     @Override
     public List<Ubicacion> list() {
         return uR.findAll();
+    }
+    @Override
+    public Optional<Ubicacion> ListarId(int id) {
+        return Optional.of(uR.findById(id).orElse(new Ubicacion()));
+    }
+
+    @Override
+    public void delete(int id){uR.deleteById(id);}
+
+    @Override
+    public List<Ubicacion> search(String nameUbicacion) {
+        return uR.search(nameUbicacion);
     }
 }
