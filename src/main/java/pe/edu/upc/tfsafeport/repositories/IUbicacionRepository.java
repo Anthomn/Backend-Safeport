6fd2f0d73d5155b583658ea6677db7b90ca69506
Anedre <anedre12345@gmail.com>
2022-10-19 10:22:13 -0500

    Creacion de Query metodo

--- src/main/java/pe/edu/upc/tfsafeport/repositories/IUbicacionRepository.java
+++ src/main/java/pe/edu/upc/tfsafeport/repositories/IUbicacionRepository.java
@@ -1,9 +1,16 @@
 package pe.edu.upc.tfsafeport.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pe.edu.upc.tfsafeport.entities.Padredefamilia;
 import pe.edu.upc.tfsafeport.entities.Ubicacion;
+
+import java.util.List;
 
 @Repository
 public interface IUbicacionRepository extends JpaRepository<Ubicacion,Integer> {
+    @Query("from Ubicacion p where p.distrito like %:distrito")
+    List<Ubicacion> search(@Param("distrito")String distrito);
 }
