af219a48218c33c7b54e85966e20efeb972cbc42
Anedre <anedre12345@gmail.com>
2022-10-19 10:20:55 -0500

    Creacion de optinal,delete,search metodos

--- src/main/java/pe/edu/upc/tfsafeport/servicesinterfaces/IUbicacionService.java
+++ src/main/java/pe/edu/upc/tfsafeport/servicesinterfaces/IUbicacionService.java
@@ -3,9 +3,14 @@ package pe.edu.upc.tfsafeport.servicesinterfaces;
 import pe.edu.upc.tfsafeport.entities.Ubicacion;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUbicacionService {
     public void insert(Ubicacion ubicacion);
 
     List<Ubicacion>list();
+    public Optional<Ubicacion> ListarId(int id);
+    public void delete(int id);
+
+    List<Ubicacion>search(String distrito);
 }
