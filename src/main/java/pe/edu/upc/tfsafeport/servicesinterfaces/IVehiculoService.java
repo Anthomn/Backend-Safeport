61cff5e3045112e2fde6e722427d00a72c8c8843
CabPerez <U20201c193@upc.edu.pe>
2022-10-22 21:53:26 -0500

    Implementacion Service de vehiculo

---
+++ src/main/java/pe/edu/upc/tfsafeport/servicesinterfaces/IVehiculoService.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.tfsafeport.servicesinterfaces;
+
+import pe.edu.upc.tfsafeport.entities.Vehiculo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IVehiculoService {
+    public void insert(Vehiculo vehiculo);
+
+    List<Vehiculo> list();
+    public Optional<Vehiculo>ListarId(int id);
+    public void delete(int id);
+
+    List<Vehiculo>search(String placa);
+}
