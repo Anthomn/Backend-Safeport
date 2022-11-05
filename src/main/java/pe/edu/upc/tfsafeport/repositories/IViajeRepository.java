2b3157673aa6c69b8bddb49a78c354e894570c5c
Anedre <anedre12345@gmail.com>
2022-11-04 19:28:51 -0500

    modificacion de viaje repository

--- src/main/java/pe/edu/upc/tfsafeport/repositories/IViajeRepository.java
+++ src/main/java/pe/edu/upc/tfsafeport/repositories/IViajeRepository.java
@@ -4,12 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pe.edu.upc.tfsafeport.entities.Vehiculo;
 import pe.edu.upc.tfsafeport.entities.Viaje;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface IViajeRepository extends JpaRepository<Viaje, Integer> {
-    @Query("from Viaje v where v.horainicio like %:horainicio")
+
+    @Query("from Viaje v where v.vehiculo.placa like %:placa%")
+    List<Viaje> buscarVehiculo(@Param("placa") String placa);
+
+    @Query("from Viaje p where p.horainicio like %:horainicio")
     List<Viaje> search(@Param("horainicio")String horainicio);
 }
