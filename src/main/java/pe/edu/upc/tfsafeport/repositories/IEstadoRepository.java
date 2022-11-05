740fffee2cf0ad6bf47f270757993fffcec9c55d
CabPerez <U20201c193@upc.edu.pe>
2022-11-04 23:51:50 -0500

    Agrege Buscar viaje buscar solicitud Buscar ubicacion entidad estado

--- src/main/java/pe/edu/upc/tfsafeport/repositories/IEstadoRepository.java
+++ src/main/java/pe/edu/upc/tfsafeport/repositories/IEstadoRepository.java
@@ -13,4 +13,13 @@ public interface IEstadoRepository extends JpaRepository<Estado,Integer> {
     @Query("from Estado e where e.direccionPartida like %:direccionPartida")
     List<Estado>search(@Param("direccionPartida")String direccionPartida);
 
+    @Query("from Estado e where e.viaje.horainicio like %:horainicio")
+    List<Estado>buscarViajes(@Param("horainicio")String horainicio);
+
+    @Query("from Estado e where e.solicitud.descripcion like %:descripcion")
+    List<Estado>buscarSolicitud(@Param("descripcion")String descripcion);
+
+    @Query("from Estado e where e.ubicacion.distrito like %:distrito")
+    List<Estado>buscarUbicacion(@Param("distrito")String distrito);
+
 }
--- src/main/java/pe/edu/upc/tfsafeport/servicesimpls/EstadoServiceImpl.java
+++ src/main/java/pe/edu/upc/tfsafeport/servicesimpls/EstadoServiceImpl.java
@@ -3,6 +3,7 @@ package pe.edu.upc.tfsafeport.servicesimpls;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pe.edu.upc.tfsafeport.entities.Estado;
 import pe.edu.upc.tfsafeport.repositories.IEstadoRepository;
 import pe.edu.upc.tfsafeport.servicesinterfaces.IEstadoService;
@@ -15,8 +16,16 @@ public class EstadoServiceImpl implements IEstadoService {
     @Autowired
     private IEstadoRepository eR;
     @Override
-    public void insert(Estado estado) {
-        eR.save(estado);
+    @Transactional
+
+    public boolean insert(Estado estado) {
+        Estado objEstado=eR.save(estado);
+        if (objEstado==null)
+        {
+            return false;
+        }
+        else
+        {return true;}
     }
 
     @Override
@@ -25,6 +34,7 @@ public class EstadoServiceImpl implements IEstadoService {
     }
 
     @Override
+    @Transactional
     public void delete(int idEstado) {
         eR.deleteById(idEstado);
     }
@@ -41,4 +51,17 @@ public class EstadoServiceImpl implements IEstadoService {
     public List<Estado> search(String direccionPartida) {
         return eR.search(direccionPartida);
     }
+    @Override
+    public List<Estado> buscarViajes(String horainicio){
+        return eR.buscarViajes(horainicio);
+    }
+    @Override
+    public List<Estado> buscarSolicitud(String descripcion){
+        return eR.buscarSolicitud(descripcion);
+    }
+    @Override
+    public List<Estado> buscarUbicacion(String distrito){
+        return eR.buscarUbicacion(distrito);
+    }
+
 }
