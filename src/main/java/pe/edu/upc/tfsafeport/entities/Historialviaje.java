bef5a3836077a8ce7741ddb82e0c6fcfd147a3b2
EgusquizaJairo <u201811623@upc.edu.pe>
2022-11-06 15:44:20 -0500

    FK-Historialviaje

--- src/main/java/pe/edu/upc/tfsafeport/entities/Historialviaje.java
+++ src/main/java/pe/edu/upc/tfsafeport/entities/Historialviaje.java
@@ -1,11 +1,15 @@
 package pe.edu.upc.tfsafeport.entities;
 
 import javax.persistence.*;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "Historialviaje")
-public class Historialviaje {
+public class Historialviaje implements Serializable {
 
+    private static final long serialVersionUID =1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idHistorial;
@@ -13,12 +17,18 @@ public class Historialviaje {
     @Column(name = "observaciones", length = 250, nullable = false)
     private String observaciones;
 
+    @ManyToOne
+    @JoinColumn(name = "idViaje",nullable = false)
+    private Viaje viaje;
+
     public Historialviaje() {
+        super();
     }
 
-    public Historialviaje(int idHistorial, String observaciones) {
+    public Historialviaje(int idHistorial, String observaciones, Viaje viaje) {
         this.idHistorial = idHistorial;
         this.observaciones = observaciones;
+        this.viaje = viaje;
     }
 
     public int getIdHistorial() {
@@ -35,5 +45,13 @@ public class Historialviaje {
 
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
+    }
+
+    public Viaje getViaje() {
+        return viaje;
+    }
+
+    public void setViaje(Viaje viaje) {
+        this.viaje = viaje;
     }
 }
